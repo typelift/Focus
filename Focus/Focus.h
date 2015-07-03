aa1443b881b28a740e7a70ed7f0d6e3acde87d15
Robert Widmann <devteam.codafi@gmail.com>
2015-07-02 18:30:48 -0600

    Initial Commit

---
+++ Focus/Focus.h
@@ -0,0 +1,19 @@
+//
+//  Focus.h
+//  Focus
+//
+//  Created by Robert Widmann on 7/2/15.
+//  Copyright © 2015 TypeLift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Focus.
+FOUNDATION_EXPORT double FocusVersionNumber;
+
+//! Project version string for Focus.
+FOUNDATION_EXPORT const unsigned char FocusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Focus/PublicHeader.h>
+
+
