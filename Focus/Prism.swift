6191b12678155d617050e765210a9f2a36aa9125
Robert Widmann <devteam.codafi@gmail.com>
2015-07-04 14:52:48 -0600

    Add Maybe Prisms

--- Focus/Prism.swift
+++ Focus/Prism.swift
@@ -6,8 +6,9 @@
 //  Copyright (c) 2015 TypeLift. All rights reserved.
 //
 
-
+/// A Prism is an `Iso` where one of the functions is partial.
 public struct Prism<S, T, A, B> {
+    ///
 	public let tryGet : S -> A?
 	public let inject : B -> T
 
@@ -16,6 +17,11 @@ public struct Prism<S, T, A, B> {
 		self.inject = inject
 	}
 
+    /// Composes a `Prism` with the receiver.
+    public func compose<I, J>(i2 : Prism<A, B, I, J>) -> Prism<S, T, I, J> {
+        return self • i2
+    }
+    
 	public func tryModify(s : S, _ f : A -> B) -> T? {
 		return tryGet(s).map(self.inject • f)
 	}
@@ -25,6 +31,10 @@ public func • <S, T, I, J, A, B>(p1 : Prism<S, T, I, J>, p2 : Prism<I, J, A, B
 	return Prism(tryGet: { p1.tryGet($0).flatMap(p2.tryGet) }, inject: p1.inject • p2.inject)
 }
 
-public func comp<S, T, I, J, A, B>(p1 : Prism<S, T, I, J>)(p2 : Prism<I, J, A, B>) -> Prism<S, T, A, B> {
-	return p1 • p2
+public func _Some<A, B>() -> Prism<A?, B?, A, B> {
+    return Prism(tryGet: identity, inject: Optional<B>.Some)
+}
+
+public func _None<A, B>() -> Prism<A?, B?, A, B> {
+    return Prism(tryGet: { _ in .None }, inject: { _ in .None })
 }
