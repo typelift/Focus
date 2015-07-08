528b0da6cba844e69787220d4aedd58774806aba
Robert Widmann <devteam.codafi@gmail.com>
2015-07-07 23:06:03 -0600

    Fix test

--- FocusTests/PrismSpec.swift
+++ FocusTests/PrismSpec.swift
@@ -12,7 +12,9 @@ import Focus
 
 class PrismSpec : XCTestCase {
     func testPrismLaws() {
-		property("modify-identity") <- forAll { (fs : IsoOf<Int, UInt>, tryGet : ArrowOf<UInt, OptionalOf<UInt>>) in
+		property("modify-identity") <- forAll { (fs : IsoOf<Int, UInt>) in
+			/// Cannot generate this in the forAll because we need this to be memoizing and consistent with the Iso.
+			let tryGet = ArrowOf<UInt, OptionalOf<UInt>>({ arc4random() % 4 == 0 ? OptionalOf(.None) : OptionalOf(.Some($0)) })
 			let prism = Prism<Int, Int, UInt, UInt>(tryGet: { $0.getOptional } • tryGet.getArrow • fs.getTo, inject: fs.getFrom)
 			return forAll { (l : Int) in
 				let m = prism.tryModify(l, { $0 })
