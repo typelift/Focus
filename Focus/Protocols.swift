fb3b114ce4ac29aa273676d781306ab8a0666a1d
Alexander Altman <alexanderaltman@me.com>
2015-07-13 13:41:53 -0700

    Begin protocols work

---
+++ Focus/Protocols.swift
@@ -0,0 +1,16 @@
+//
+//  Protocols.swift
+//  Focus
+//
+//  Created by Alexander Ronald Altman on 7/13/15.
+//  Copyright © 2015 TypeLift. All rights reserved.
+//
+
+import Foundation
+
+public protocol OpticFamilyType {
+	typealias Source
+	typealias Target
+	typealias AltSource
+	typealias AltTarget
+}
