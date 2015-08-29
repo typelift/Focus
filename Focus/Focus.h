a29e5ee1ab94f5c80c0c067da66c4e579c69ae29
Alexander Altman <alexanderaltman@me.com>
2015-08-28 17:44:52 -0700

    Remove version numbers from header
    also include header in both targets

--- Focus/Focus.h
+++ Focus/Focus.h
@@ -6,14 +6,4 @@
 //  Copyright © 2015 TypeLift. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for Focus.
-FOUNDATION_EXPORT double FocusVersionNumber;
-
-//! Project version string for Focus.
-FOUNDATION_EXPORT const unsigned char FocusVersionString[];
-
 // In this header, you should import all the public headers of your framework using statements like #import <Focus/PublicHeader.h>
-
-
