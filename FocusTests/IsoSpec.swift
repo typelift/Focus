1dfc83af6d0fe522a84170702d1136d509f966a7
Robert Widmann <devteam.codafi@gmail.com>
2015-12-21 23:48:53 -0500

    Test inverse Iso laws

--- FocusTests/IsoSpec.swift
+++ FocusTests/IsoSpec.swift
@@ -41,6 +41,37 @@ class IsoSpec : XCTestCase {
 			return iso1.inject(iso2.inject(x)) == isoC.inject(x)
 		}
 	}
+
+	func testIsoInverseLaws() {
+		property("around the world") <- forAll { (x : UInt, fs : IsoOf<Int, UInt>) in
+			let iso = Iso(get: fs.getTo, inject: fs.getFrom).invert.invert
+			return iso.get(iso.inject(x)) == x
+		}
+
+		property("and back again") <- forAll { (x : Int, fs : IsoOf<Int, UInt>) in
+			let iso = Iso(get: fs.getTo, inject: fs.getFrom).invert.invert
+			return iso.inject(iso.get(x)) == x
+		}
+
+		property("modify-identity") <- forAll { (x : Int, fs : IsoOf<Int, UInt>) in
+			let iso = Iso<Int, Int, UInt, UInt>(get: fs.getTo, inject: fs.getFrom).invert.invert
+			return iso.modify(x, { $0 }) == x
+		}
+
+		property("compose is associative (get)") <- forAll { (x : Int, fs : IsoOf<Int, Int>, gs : IsoOf<Int, Int>) in
+			let iso1 = Iso(get: fs.getTo, inject: fs.getFrom).invert.invert
+			let iso2 = Iso(get: gs.getTo, inject: gs.getFrom).invert.invert
+			let isoC = iso1.compose(iso2)
+			return iso2.get(iso1.get(x)) == isoC.get(x)
+		}
+
+		property("compose is associative (inject)") <- forAll { (x : Int, fs : IsoOf<Int, Int>, gs : IsoOf<Int, Int>) in
+			let iso1 = Iso(get: fs.getTo, inject: fs.getFrom).invert.invert
+			let iso2 = Iso(get: gs.getTo, inject: gs.getFrom).invert.invert
+			let isoC = iso1.compose(iso2)
+			return iso1.inject(iso2.inject(x)) == isoC.inject(x)
+		}
+	}
 }
 
 
@@ -71,6 +102,37 @@ class SimpleIsoSpec : XCTestCase {
 		property("compose is associative (inject)") <- forAll { (x : Int, fs : IsoOf<Int, Int>, gs : IsoOf<Int, Int>) in
 			let iso1 = SimpleIso(get: fs.getTo, inject: fs.getFrom)
 			let iso2 = SimpleIso(get: gs.getTo, inject: gs.getFrom)
+			let isoC = iso1.compose(iso2)
+			return iso1.inject(iso2.inject(x)) == isoC.inject(x)
+		}
+	}
+
+	func testIsoInverseLaws() {
+		property("around the world") <- forAll { (x : UInt, fs : IsoOf<Int, UInt>) in
+			let iso = SimpleIso(get: fs.getTo, inject: fs.getFrom).invert.invert
+			return iso.get(iso.inject(x)) == x
+		}
+
+		property("and back again") <- forAll { (x : Int, fs : IsoOf<Int, UInt>) in
+			let iso = SimpleIso(get: fs.getTo, inject: fs.getFrom).invert.invert
+			return iso.inject(iso.get(x)) == x
+		}
+
+		property("modify-identity") <- forAll { (x : Int, fs : IsoOf<Int, UInt>) in
+			let iso = Iso<Int, Int, UInt, UInt>(get: fs.getTo, inject: fs.getFrom).invert.invert
+			return iso.modify(x, { $0 }) == x
+		}
+
+		property("compose is associative (get)") <- forAll { (x : Int, fs : IsoOf<Int, Int>, gs : IsoOf<Int, Int>) in
+			let iso1 = SimpleIso(get: fs.getTo, inject: fs.getFrom).invert.invert
+			let iso2 = SimpleIso(get: gs.getTo, inject: gs.getFrom).invert.invert
+			let isoC = iso1.compose(iso2)
+			return iso2.get(iso1.get(x)) == isoC.get(x)
+		}
+
+		property("compose is associative (inject)") <- forAll { (x : Int, fs : IsoOf<Int, Int>, gs : IsoOf<Int, Int>) in
+			let iso1 = SimpleIso(get: fs.getTo, inject: fs.getFrom).invert.invert
+			let iso2 = SimpleIso(get: gs.getTo, inject: gs.getFrom).invert.invert
 			let isoC = iso1.compose(iso2)
 			return iso1.inject(iso2.inject(x)) == isoC.inject(x)
 		}
