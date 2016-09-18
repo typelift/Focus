2f1748d7a31989ffed08e3955539de22f7c4ad74
Robert Widmann <devteam.codafi@gmail.com>
2016-09-18 13:11:26 -0400

    Reorganize for package manager support

--- Focus/Focus.h
+++
@@ -1,9 +0,0 @@
-//
-//  Focus.h
-//  Focus
-//
-//  Created by Robert Widmann on 7/2/15.
-//  Copyright © 2015 TypeLift. All rights reserved.
-//
-
-// In this header, you should import all the public headers of your framework using statements like #import <Focus/PublicHeader.h>
